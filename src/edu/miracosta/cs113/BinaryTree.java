afeed033b559ecfe309d5bb5ba426b6daa2296d5
ArturoCrespo <c.arto737@gmail.com>
2022-05-11 20:56:05 -0700

    All BinaryTreeTest and MorseCodeTreeTests pass.

---
+++ src/edu/miracosta/cs113/BinaryTree.java
@@ -0,0 +1,139 @@
+package edu.miracosta.cs113 ;
+
+import java.util.Scanner;
+
+public class BinaryTree<E> {
+
+    protected Node<E> root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    protected BinaryTree(Node<E> newRoot) {
+        root = newRoot;
+    }
+
+    public BinaryTree(E data, BinaryTree<E> leftTree, BinaryTree<E> rightTree) {
+        root = new Node<>(data);
+        if (leftTree != null) {
+            root.left = leftTree.root;
+        } else {
+            root.left = null;
+        }
+        if (rightTree != null) {
+            root.right = rightTree.root;
+        } else {
+            root.right = null;
+        }
+    }
+
+    /**
+     *
+     * @return E - The data stored at the root of the binary tree
+     */
+    public E getData() {
+        return root.data;
+    }
+
+    /**
+     * @return BinaryTree<E> - Tree representing the left subtree starting at the root
+     */
+    public BinaryTree<E> getLeftSubtree() {
+        if (root != null && root.left != null) {
+            return new BinaryTree<>(root.left);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @return BinaryTree<E> - Tree representing the right subtree starting at the root
+     */
+    public BinaryTree<E> getRightSubtree() {
+        if (root != null && root.right != null) {
+            return new BinaryTree<>(root.right);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @return boolean value representing whether or not the current node is a leaf node
+     */
+    public boolean isLeaf() {
+        return (root.left == null && root.right == null);
+    }
+
+    /**
+     * @return - String value representing the Binary Tree
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        preOrderTraverse(root, 1, sb);
+        return sb.toString();
+    }
+
+    /**
+     * Recursive helper method for toString. Traverses binary tree with
+     * a pre-order traversal
+     *
+     * @param node  - Current node traversal is on
+     * @param depth - Which level the traversal is at (Used for spacing the output)
+     * @param sb    - StringBuilder for appending each successive node
+     */
+    private void preOrderTraverse(Node<E> node, int depth, StringBuilder sb) {
+        for (int i = 1; i < depth; i++) {
+            sb.append(" ");
+        }
+        if (node == null) {
+            sb.append("null\n");
+        } else {
+            sb.append(node.toString());
+            sb.append("\n");
+            preOrderTraverse(node.left, depth + 1, sb);
+            preOrderTraverse(node.right, depth + 1, sb);
+        }
+    }
+
+    /**
+     * Recursive method for parsing a binary tree from its String representation
+     *
+     * @param scan - Scanner object which points to a file containing data for binary tree
+     * @return     - Binary tree read from Scanner
+     */
+    public static BinaryTree<String> readBinaryTree(Scanner scan) {
+        String data = scan.nextLine().trim();
+        if (data.equals("null")) {
+            return null;
+        } else {
+            BinaryTree<String> leftTree = readBinaryTree(scan);
+            BinaryTree<String> rightTree = readBinaryTree(scan);
+            return new BinaryTree<>(data, leftTree, rightTree);
+        }
+    }
+
+    protected static class Node<E> {
+        protected E data;
+        protected Node<E> left;
+        protected Node<E> right;
+
+        public Node() {
+            this(null);
+        }
+
+        public Node(E newData) {
+            data = newData;
+            left = null;
+            right = null;
+        }
+
+        public String toString() {
+            if (data == null) {
+                return "";
+            }
+            return data.toString();
+        }
+    }
+}
---
+++ src/edu/miracosta/cs113/MorseCodeTree.java
@@ -0,0 +1,122 @@
+package edu.miracosta.cs113;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * MorseCodeTree : A BinaryTree, with Nodes of type Character to represent each letter of the English alphabet,
+ * and a means of traversal to be used to decipher Morse code.
+ *
+ * @version 1.0
+ */
+public class MorseCodeTree extends BinaryTree<Character> {
+
+    public static final File INPUT_FILE = new File("/Users/arturo/Desktop/CodeRepository/Trees_HW7/src/edu/miracosta/cs113/Letters.txt") ;
+
+    public MorseCodeTree() {
+        readMorseCodeTree() ;
+    }
+
+    // TODO:
+    // Build this class, which includes the parent BinaryTree implementation in addition to
+    // the `translateFromMorseCode` and `readMorseCodeTree` methods. Documentation has been suggested for the former,
+    // where said exceptional cases are to be handled according to the corresponding unit tests.
+
+    /**
+     * Non-recursive method for translating a String comprised of morse code values through traversals
+     * in the MorseCodeTree.
+     *
+     * The given input is expected to contain morse code values, with '*' for dots and '-' for dashes, representing
+     * only letters in the English alphabet.
+     *
+     * This method will also handle exceptional cases, namely if a given token's length exceeds that of the tree's
+     * number of possible traversals, or if the given token contains a character that is neither '*' nor '-'.
+     *
+     * @param morseCode The given input representing letters in Morse code
+     * @return a String representing the decoded values from morseCode
+     */
+    public String translateFromMorseCode(String morseCode) {
+
+        if (morseCode.isEmpty()) {
+            throw new IllegalArgumentException("Illegal argument (null) provided to translateFromMorseCode method");
+        }
+
+        String[] splitString = morseCode.split(" ") ;
+
+        Node<Character> currentNode ;
+
+        StringBuilder decodedString = new StringBuilder() ;
+
+        for (String letter : splitString) {
+
+            currentNode = root;
+
+            for (int i = 0 ; i < letter.length() ; i++) {
+                if (letter.charAt(i) == '*') {
+                    currentNode = currentNode.left ;
+                } else if (letter.charAt(i) == '-' ) {
+                    currentNode = currentNode.right ;
+                } else {
+                    throw new IllegalArgumentException("String may only contain * or -") ;
+                }
+            }
+
+            decodedString.append(currentNode.data) ;
+        }
+        return decodedString.toString() ;
+    }
+
+    private void readMorseCodeTree() {
+
+        //initialize root to null
+        root = new Node<>(null) ;
+
+        //create an arraylist of all the data in the text file
+        ArrayList<String> unparsedCode = new ArrayList<String>() ;
+
+        //create a Node to contain the character data form the Strings in text file
+        Node<Character> currentNode ;
+
+        try {
+            Scanner input = new Scanner(INPUT_FILE) ;
+
+            while(input.hasNext()) {
+                unparsedCode.add(input.nextLine()) ;
+            }
+            input.close();
+        } catch(FileNotFoundException fnfe) {
+            fnfe.printStackTrace() ;
+            System.err.println("ERROR: Cannot open file: " + INPUT_FILE + " for input.") ;
+        }
+
+        for(String l : unparsedCode) {
+            currentNode = root ;
+
+            char letter = l.charAt(0) ;
+            String morseCode = l.substring(2) ;
+
+            for (int i = 0 ; i < morseCode.length() ; i++) {
+
+                //left
+                if (morseCode.charAt(i) == '*') {
+                    if (currentNode.left == null) {
+                        currentNode.left = new Node<>(letter) ;
+                    } else {
+                        currentNode = currentNode.left ;
+                    }
+
+                //right
+                } else if (morseCode.charAt (i) == '-') {
+                    if (currentNode.right == null) {
+                        currentNode.right = new Node<>(letter);
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+        }
+    }
+
+} // End of class MorseCodeTree
